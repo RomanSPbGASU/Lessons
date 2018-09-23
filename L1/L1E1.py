5b22136d55a0103ffc6ff03fe9e48d8ce460a7e3
Evsin Roman <evsinroman@yandex.ru>
2018-09-23 18:24:14 +0300

    Готово 2 задания из 1-го урока

---
+++ L1/L1E1.py
@@ -0,0 +1,23 @@
+try:
+    initialAmount = float(input("Введите начальную сумму, ₽ = "))
+    rateAmount = float(input("Введите размер ставки, % = "))
+    investmentTerm = int(input("Введите срок инвестирования, лет = "))
+except:
+    print("Ошибка ввода")
+    print("Начальная сумма = 100000 ₽")
+    print("Размер ставки = 10%")
+    print("Срок инвестирования = 5 лет")
+    initialAmount, rateAmount, investmentTerm = 100000, 10, 5
+finalAmountSimple = initialAmount * (1 + rateAmount / 100 * investmentTerm)
+finalAmountComplicated = initialAmount * (1 + rateAmount / 100) ** investmentTerm
+print("\t\t\t Простой процент Сложный процент")
+print("Начальная сумма, ₽:\t", round(initialAmount, 1), "\t", initialAmount)
+print("Конечная сумма, ₽:\t", round(finalAmountSimple, 1), "\t", round(finalAmountComplicated, 1))
+incomeSimple = finalAmountSimple - initialAmount
+incomeComplicated = finalAmountComplicated - initialAmount
+print("Доход, руб:\t\t", round(incomeSimple, 1), "\t", round(incomeComplicated, 1))
+def getYield(income, initial_amount, term):
+    return income/initial_amount*1/term*100
+yieldSimple = getYield(incomeSimple, initialAmount, investmentTerm)
+yieldComplicated = getYield(incomeComplicated, initialAmount, investmentTerm)
+print("Доходность, % год.:\t", round(yieldSimple, 1), "\t\t", round(yieldComplicated, 1))
---
+++ L1/L1E2.1.py
@@ -0,0 +1,50 @@
+#varin = {"initial": None, "rate": None, "term": None}
+func1 = lambda : float(input("Введите начальную сумму, ₽ = "))
+func2 = lambda : float(input("Введите размер ставки, % = "))
+func3 = lambda : float(input("Введите срок инвестирования, лет = "))
+funclist = [func1, func2, func3]
+print(funclist)
+print(type(funclist))
+print(funclist[1]())
+#def getinitial
+#for val in varin:
+#    varin[val] = float(input("Введите начальную сумму, ₽ = "))
+#    print(type(varin["initial"]))
+
+
+
+
+
+
+
+#rateAmount = float(input("Введите размер ставки, % = "))
+#investmentTerm = int(input("Введите срок инвестирования, лет = "))
+#except ValueError:
+#    print("Ошибка ввода. Будут использованы значения по умолчанию:")
+#    print("Начальная сумма = 100000 ₽")
+#    print("Размер ставки = 10%")
+#    print("Срок инвестирования = 5 лет")
+#    initialAmount, rateAmount, investmentTerm = 100000, 10, 5
+#for i in range(90):
+#    print("_", end = "")
+#print()
+#print("\t\t\t\t\tСхема начисления процентов")
+#print("\t\t\t Ежемесячно\tЕжеквартально\tРаз в полгода\tЕжегодно")
+#print("Начальная сумма, ₽:\t " , end = "")
+#for i in range(4):
+#    print(float(round(initialAmount, 1)), end = "\t")
+#print()
+#def getFinalAmount(monthInPeriod):
+#    return initialAmount * (1 + rateAmount / 100 * monthInPeriod / 12) ** (investmentTerm * 12 / monthInPeriod)
+#MONTHLY, QUARTERLY, HALFYEARLY, YEARLY = 1, 3, 6, 12
+#finalAmountMonthly = getFinalAmount(MONTHLY)
+#finalAmountQuarterly = getFinalAmount(QUARTERLY)
+#finalAmountHalfyearly = getFinalAmount(HALFYEARLY)
+#finalAmountYearly = getFinalAmount(YEARLY)
+#print("Конечная сумма, ₽:\t ", round(finalAmountMonthly, 2), "\t", round(finalAmountQuarterly, 2), "\t", round(finalAmountHalfyearly, 2), "\t", round(finalAmountYearly, 2), sep = "")
+#incomeMonthly, incomeQuarterly, incomeHalfyearly, incomeYearly = finalAmountMonthly - initialAmount, finalAmountQuarterly - initialAmount, finalAmountHalfyearly - initialAmount, finalAmountYearly - initialAmount
+#print("Доход, руб:\t\t ", round(incomeMonthly, 2), "\t", round(incomeQuarterly, 2), "\t", round(incomeHalfyearly, 2), "\t", round(incomeYearly, 2), sep = "")
+#def getYield(income):
+#    return income / initialAmount * 1 / investmentTerm * 100
+#yieldMonthly, yieldQuarterly, yieldHalfyearly, yieldYearly = getYield(incomeMonthly), getYield(incomeQuarterly), getYield(incomeHalfyearly), getYield(incomeYearly)
+#print("Доходность, % год.:\t ", round(yieldMonthly, 2), "\t\t", round(yieldQuarterly, 2), "\t\t", round(yieldHalfyearly, 2), "\t\t", round(yieldYearly, 2), sep = "")
