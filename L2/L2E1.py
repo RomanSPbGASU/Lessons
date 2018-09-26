e011c422cf0691d3a3c75b4ec8c0797cebd1b473
Evsin Roman <evsinroman@yandex.ru>
2018-09-26 18:47:15 +0300

    Сделаны 2 урока

--- L2/L2E1.py
+++ L2/L2E1.py
@@ -1,26 +1,27 @@
-indict = {"initial": 50000, "rate": 7.5, "term": 15}
-#funcinlist = [lambda : float(input("Введите начальную сумму, ₽ = ")),
-#              lambda : float(input("Введите размер ставки, % год = ")),
-#              lambda : int(input("Введите срок инвестирования, лет = "))]
-#i = 0
-#for key in indict:   # добьёмся от пользователя ввода корректных данных
-#    while 1:
-#        try:
-#            indict[key] = funcinlist[i]()
-#            i += 1
-#            break
-#        except:
-#            print("\tОшибка. Некорректный ввод")
+indict = {"initial": None, "rate": None, "term": None}
+funcinlist = [lambda : float(input("Введите начальную сумму, ₽ = ")),
+              lambda : float(input("Введите размер ставки, % год = ")),
+              lambda : int(input("Введите срок инвестирования, лет = "))]
+i = 0
+for key in indict:   # добьёмся от пользователя ввода корректных данных
+    while 1:
+        try:
+            indict[key] = funcinlist[i]()
+            i += 1
+            break
+        except:
+            print("\tОшибка. Некорректный ввод")
 print("\nНачальная сумма = %.2f рублей" % indict["initial"])
 print("Процентная ставка = %.2f %% год" % indict["rate"])
 print("Срок инвестирования = %d лет" % indict["term"])
-print("\t\t\t\t\t\t Простой процент\t   Сложный процент")
-print("Сумма, руб.   Доход за год, руб.   Сумма, руб.   Доход за год, руб.")
+print("\t" * 2, "Простой процент\t\t Сложный процент")
+print("\t" * 2, "Сумма, руб. Доход за год, руб.  Сумма, руб. Доход за год, руб.")
 simple = complex = indict["initial"]
 for i in range(indict["term"]):
     in_sim = simple
     in_com = complex
-    simple *=  (1 + indict["rate"] / 100 * indict["term"])
-    complex *= (1 + indict["rate"] / 100) ** indict["term"]
-    in_sim -= -simple
-    print("%d-й год" % i, simple, in_sim, complex, in_com)
+    simple += indict["initial"] * indict["rate"] / 100
+    complex *= (1 + indict["rate"] / 100)
+    in_sim = simple - in_sim
+    in_com = complex - in_com
+    print("%d-й год \t" % i, round(simple, 2), "\t", round(in_sim, 2), "\t", round(complex, 2), "\t", round(in_com, 2))
