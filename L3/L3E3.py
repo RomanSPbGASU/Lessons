a2e48e86aaf6f8fa8f5cf4ab9b640f52c99a1b62
Evsin Roman <evsinroman@yandex.ru>
2018-10-02 16:00:09 +0300

    Немного поменял форматирование в 3-м задании

--- L3/L3E3.py
+++ L3/L3E3.py
@@ -1,27 +1,26 @@
-indict = {"initial": None, "rate": None, "term": None}
-funcinlist = [lambda : float(input("Введите начальную сумму, ₽ = ")),
-              lambda : float(input("Введите размер ставки, % год = ")),
-              lambda : int(input("Введите срок инвестирования, лет = "))]
-i = 0
-for key in indict:   # добьёмся от пользователя ввода корректных данных
-    while 1:
-        try:
-            indict[key] = funcinlist[i]()
-            i += 1
-            break
-        except:
-            print("\tОшибка. Некорректный ввод")
-print("\nНачальная сумма = %.2f рублей" % indict["initial"])
-print("Процентная ставка = %.2f %% год" % indict["rate"])
-print("Срок инвестирования = %d лет" % indict["term"])
-print("\t" * 2, "Простой процент\t\t Сложный процент")
-print("\t" * 2, "Сумма, руб. Доход за год, руб.  Сумма, руб. Доход за год, руб.")
-simple = complex = indict["initial"]
-for i in range(indict["term"]):
+#itext = ("Введите начальную сумму, ₽ = ", "Введите размер ставки, % = ", "Введите срок инвестирования, лет = ")
+#t = (float, float, int)
+#var = []; i = 0
+#while i <= 2:
+#    try:
+#      var.append(t[i](input(itext[i])))
+#    except (ValueError):
+#        print("Ошибка. Некорректный ввод")
+#        continue
+#    else:
+#        i += 1
+init, rate, term = 10000, 15, 5
+print("\nНачальная сумма = %.2f рублей" % init)
+print("Процентная ставка = %.2f %% год" % rate)
+print("Срок инвестирования = %d лет" % term)
+print(("{:<{}}" + "{:<{}}" * 2).format("", 8, "Простой процент", 30, "Сложный процент", 30))
+print("".format("Сумма, руб.", "Доход за год, руб.", "Сумма, руб.", "Доход за год, руб."))
+simple = complex = init
+for i in range(term):
     in_sim = simple
     in_com = complex
-    simple += indict["initial"] * indict["rate"] / 100
-    complex *= (1 + indict["rate"] / 100)
+    simple += init * rate / 100
+    complex *= (1 + rate / 100)
     in_sim = simple - in_sim
     in_com = complex - in_com
     print("%d-й год \t" % i, round(simple, 2), "\t", round(in_sim, 2), "\t", round(complex, 2), "\t", round(in_com, 2))
