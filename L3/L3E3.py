27b25f1f063e2551aaee5ec1512f665b29a90439
Evsin Roman <evsinroman@yandex.ru>
2018-10-02 19:48:47 +0300

    Форматирование в третьем задании работает, но алгоритм вычисления не правильный

--- L3/L3E3.py
+++ L3/L3E3.py
@@ -13,8 +13,13 @@ init, rate, term = 10000, 15, 5
 print("\nНачальная сумма = %.2f рублей" % init)
 print("Процентная ставка = %.2f %% год" % rate)
 print("Срок инвестирования = %d лет" % term)
-print(("{:<{}}" + "{:<{}}" * 2).format("", 8, "Простой процент", 30, "Сложный процент", 30))
-print("".format("Сумма, руб.", "Доход за год, руб.", "Сумма, руб.", "Доход за год, руб."))
+col_w = 18  # ширина основных столбцов
+print(("{:<{}}" + "{:^{}}" * 2).format("", col_w/2, "Простой процент", col_w*2, "Сложный процент", col_w*2), end = "")
+def fm_print(*tuple):
+    col_width = [col_w/2, *[col_w] * 4]
+    print(col_width)
+    print(("{:>{}}" + "{:^{}}" * 4).format(*tuple))
+fm_print("", col_w/2, *(("Сумма, руб.", col_w, "Доход за год, руб.", col_w) * 2))
 simple = complex = init
 for i in range(term):
     in_sim = simple
@@ -23,5 +28,5 @@ for i in range(term):
     complex *= (1 + rate / 100)
     in_sim = simple - in_sim
     in_com = complex - in_com
-    print("%d-й год \t" % i, round(simple, 2), "\t", round(in_sim, 2), "\t", round(complex, 2), "\t", round(in_com, 2))
+    print("%d-й год \t" % (i + 1), round(simple, 2), "\t", round(in_sim, 2), "\t", round(complex, 2), "\t", round(in_com, 2))
 
