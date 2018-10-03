7e762fe6bcf6a1782514fb70a973c792ba98ecc5
Evsin Roman <evsinroman@yandex.ru>
2018-10-04 01:01:18 +0300

    Сделал 2-й вариант 5-го задания Начал 6-е задание

--- L3/L3E5.1.py
+++ L3/L3E5.1.py
@@ -1,19 +1,18 @@
-from re import compile, search
+from re import compile, search, match
 print("Введите слово 'stop' для получения результата")
 res = 0
 while 1:
-    ptrn = re.compile(r"[+-]*[0-9]+") # шаблон для ввода числа в C++ стиле
-    stop = re.compile(r"stop")
+    ptrn = compile(r"[+-]*[0-9]+") # шаблон для ввода числа в C++ стиле
+    stop = compile(r"stop")
     try:
         inp = input("Введите целое число: ")
-        num = ptrn.search(inp)
-        if num != None:
-            res += num
-        if inp == '':
-            raise IOError()
         if stop.search(inp):
            break
-    except ValueError:
+        if inp == '':
+            raise IOError()
+        num = int(ptrn.match(inp).string)
+        res += num
+    except (ValueError, AttributeError):
         print("Необходимо ввести число, а не строку!!!")
     except IOError:
         print("Вы не ввели значение!")
--- L3/L3E6.py
+++ L3/L3E6.py
@@ -1,3 +1,5 @@
+from re import compile, search
+ptrn = re.compile(r".*[\./]\d{4}$")
 while 1:
     date = input("Введите дату в одном из двух форматов: 10.09.1995 или 10/09/1995")
-    print("Дата введена не/правильно")
+    print("Дата введена %sправильно" % "" if ptrn.search(date) else "не")
