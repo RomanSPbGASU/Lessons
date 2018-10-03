4a3f7652bc49fc7b6d49023c203698fac684f880
Evsin Roman <evsinroman@yandex.ru>
2018-10-03 23:59:58 +0300

    Выполнил 5-е задание первым способом

--- L3/L3E5.py
+++ L3/L3E5.py
@@ -1,3 +1,16 @@
-print("Введите слово stop для получения результата")
-num = input("Введите целое число: ")
-
+print("Введите слово 'stop' для получения результата")
+res = 0
+while 1:
+    try:
+        inp = input("Введите целое число: ")
+        if inp == '':
+            raise IOError()
+        if inp == "stop":
+           break
+        num = int(inp)
+        res += num
+    except ValueError:
+        print("Необходимо ввести число, а не строку!!!")
+    except IOError:
+        print("Вы не ввели значение!")
+print("Сумма введённых чисел равна: ", res)
