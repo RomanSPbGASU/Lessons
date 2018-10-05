d4b7441c5dda3d4da6c3af168ec8d50d3b65c5fd
Evsin Roman <evsinroman@yandex.ru>
2018-10-05 20:26:58 +0300

    Дописал 3-е задание 4-го урока. Не отлаживал.

--- L4/L4E3.py
+++ L4/L4E3.py
@@ -2,20 +2,25 @@ import random
 
 
 class guess_the_word():
-    """ Консольная игра по типу "Поля чудес" """
+    """Класс консольной игры по типу "Поля чудес" """
 
-
-    def __init__:
+    def __init__(self, word = "example", dif_level = 5):
         print("{:^80}".format("Игра: Угадайте слово"))
         try:
             file = open('word_rus.txt', 'r')
         except Exception as ex:
             print("Ошибка: %s" % ex.args[0])
             raise SystemExit
+        rus_list = []
+        for word in file:
+            rus_list.append(word)
         self.dif_level = 5
+        self.cage = ""
+        self.letters = ""
+        self.mistakes = 0
 
     def put_dif_level(self):
-    """ Диалог выбора уровня сложности (от 0 до 10) """
+        """Метод выводит диалог выбора уровня сложности (от 0 до 10). """
         try:
             dif_level = int(input("Выберите уровень сложности (0-10): "))
             if dif_level < 0 or dif_level > 10:
@@ -26,34 +31,57 @@ class guess_the_word():
             raise Exception("Ошибка. Необходимо ввести число. ")
         mis_max = (33 - len({word})) * (10 - dif_level) / 10
 
+    def choose_word(self, word):
+        if not word:
+            raise Exception("Слово для игры не выбрано")
+        else:
+            cage = ""
+            for letter in word:
+                cage += "*"
 
+    def choose_rnd_word(self):
+        """Метод выбирает случайное слово из словаря."""
+        word = random.choice(rus_list)
+        choose_word(word)
 
     def show_cage(self):
+        print("{:^80}".format(cage))
+
+    def update_cage(letter):
+        for i, lt in enumerate(word):
+            if lt == letters[-1]:
+                cage = cage[0:i] + lt + cage[i:-1]
 
     def put_letter(self):
+        """Метод выводит диалог о вводе предполагаемой буквы. """
+        letters += str(input("Введите букву: ").lower())
+        if letters[-1] in word:
+            update_cage(letters[-1])
+        else:
+            mistakes += 1
+            print("Буквы '%s' в этом слове нет. Количество ошибок: %d / %d" % (letter, mistakes, mis_max))
 
-        rus_list = []
-        for word in file:
-            rus_list.append(word)
-        word = random.choice(rus_list)
-        ans = ""
-        for letter in word:
-            ans += "*"
-        letters = ""
-        mistakes = 0
-        #while mistakes < len(word) / 33 * 20
-        mis_max = (33 - len({word}))*(10 - dif_level)/10
-        while mistakes < mis_max:
-            print(ans)
-            letters += str(input("Введите букву: ").lower())
-            if letters[-1] in word:
-                for i, lt in enumerate(word):
-                    if lt == letters[-1]:
-                        ans = ans[0:i] + lt + ans[i:-1]
-                print(ans)
+    def again(self):
+        return input("Сыграем ещё раз? (д/н): ").lower() != "н"
+
+    def play_game(self):
+        while 1:
+            print("Угадайте слово")
+            choose_rnd_word()
+            show_cage()
+            put_dif_level()
+            while mistakes <= mis_max:
+                put_letter()
+                show_cage()
+                if word == cage:
+                    print("Вы выиграли!")
+                    break
             else:
-                mistakes += 1
-                print("Буквы '%s' в этом слове нет. Количество ошибок: %d" % (letter, mistakes))
-        again = input("Сыграем ещё раз? (д/н): ").lower() != "н"
-        if not again:
-            break
+                print("Вы проиграли")
+            if not again():
+                break
+
+
+if __name__ == "__main__":
+    game = guess_the_word()
+    game.play_game()
