373f44f20b4489f5cd4351569c8166188cb76d31
Роман Евсин <romanevsin@yandex.ru>
2018-10-08 18:39:25 +0300

    Выполнил задание 7 из 3-го урока. Работает упрощённая проверка вводимых адресов электронной почты. Подправил 6-е задание.

--- L3/L3E7.py
+++ L3/L3E7.py
@@ -1,3 +1,9 @@
-email = input("Введите адрес электронной почты: ")
-print("e-mail \"\" соответствует шаблону")
-print("логин: домен:")
+from re import compile, search
+
+
+# проверка e-mail (упрощённая)
+ptrn = compile(r"(?P<box>[a-z0-9]+[a-z0-9_.-]*[a-z0-9])@(?P<host>[a-z]+-?[a-z]*\.[a-z]+)")
+string = input("Введите адрес электронной почты: ")
+email = ptrn.search(string)
+print("e-mail '%s' соответствует шаблону" % email.group())
+print("логин: %s домен: %s" % (email.group("box"), email.group("host")))
