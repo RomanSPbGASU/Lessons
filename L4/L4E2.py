ae284a086ee1971ded3291592ad1f56f9668582c
Роман Евсин <romanevsin@yandex.ru>
2018-10-08 20:00:31 +0300

    Выполнил 2-е задание 4-го урока

--- L4/L4E2.py
+++ L4/L4E2.py
@@ -1,1 +1,13 @@
+from re import compile, search
 
+
+init = ["    Газпром     ", "   Роснефть!", "ЛУКойл#", "Сургутнефтегаз", "      Сбербанк ?  ", "транснефть", "$МосЭнерго@"]
+print("Начальный список:")
+for name in init:
+    print(name)
+
+ptrn = compile(r"\s*([а-яА-Я]+).*")
+print("\nОчищенный список:")
+form = [ptrn.search(name).group(1).capitalize() for name in init if ptrn.search(name) != None]
+for name in form:
+    print(name)
