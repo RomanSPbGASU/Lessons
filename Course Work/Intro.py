dff24d9075d5cc29fe0242c31294e4dee7a0eda2
Evsin Roman <evsinroman@yandex.ru>
2018-10-12 21:15:15 +0300

    Добавил в класс Neuron несколько методов

--- Course Work/Intro.py
+++ Course Work/Intro.py
@@ -23,21 +23,45 @@ class Neuron:
     def __init__(self, name, *in_arr):
         #if len(in_arr) != 900:
         #    raise valueerror("входной массив должен содержать 30x30 элементов")
-        self.__name = name
-        self.__in_arr = in_arr
-        self.__memory = []
-        self.__out_arr = []
+        self.__name = str(name)
+        self.__in_arr = bytearray(in_arr)
+        self.__memory = bytearray()
+        self.__out_arr = bytearray()
 
-    def __format__(self):
-        return self.__name
+    def __repr__(self):
+        return str(self.__name)
+
+    def __str__(self):
+        return str("Нейрон")
+
+    def save(self, directory):
+        file = open(self.__name, "wb")
+        for byte in self.__memory:
+            file.write(byte)
+
+    def read_data(self, filename):
+        file = open(filename)
+        for byte in file:
+            self.__memory += byte
+
+    def to_png(self):
+        signature = b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"
+        ihdr = b"\x00\x00\x00\x0D\x49\x48\x44\x52\x00\x00\x00\x1E\x00\x00\x00\x1E\x08\x02\x00\x00\x00\xFC\x18\xED\xA3"
+        idat = b""
+        iend = b"\x00\x00\x00\x00\x49\x45\x4E\x44\xAE\x42\x60\x82"
 
 
 if __name__ == "__main__":
 
-    neurons = []
-    for i in range(33):
-        neurons.append(Neuron(chr(0x410 + i)))
-    print(neurons)
+    #neurons = []
+    #for i in range(33):
+    #    neurons.append(Neuron("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"[i]))
+    #print(neurons)
 
-    file = open("K_letter.png", "r")
-    letter = file.re
+    #file = open("K_letter.png", "rb")
+    #letter = file.read()
+    #print(letter)
+
+    neuron = Neuron("Example")
+    neuron.save("D:/Desktop/Python/Lessons Python/Course Work/")
+
