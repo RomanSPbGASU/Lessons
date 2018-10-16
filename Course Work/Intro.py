e1b74ebeb9e533da5f85c6af59fc13eb1c028b7d
Evsin Roman <evsinroman@yandex.ru>
2018-10-16 19:58:53 +0300

    Создал класс для пользовательского интерфейса

--- Course Work/Intro.py
+++ Course Work/Intro.py
@@ -1,4 +1,7 @@
 from PIL import Image, ImageDraw, BmpImagePlugin as BIP
+import tkinter as tk
+import tkinter.filedialog as fd
+import tkinter.messagebox as mb
 
 
 class Neuron:
@@ -37,6 +40,20 @@ class Neuron:
         pix = image.load()
 
 
+class GUI(tk.Tk):
+    def __init__(self):
+        self = tk.Tk()
+        self.title("Распознование символов на Python")
+        self.geometry("500x250+200+300")
+
+    def show_file_dialog(self):
+        path = fd.askopenfilename(initialdir="",
+                                  title="Выберите файл для распознавания",
+                                  filetypes=(("BMP image files (*.bmp)", "*.bmp"),
+                                             ("", "")))
+        self.filename = path.split(sep="/")[-1]
+
+
 if __name__ == "__main__":
     # neurons = []
     # for i in range(33):
@@ -47,6 +64,23 @@ if __name__ == "__main__":
     # letter = file.read()
     # print(letter)
 
+    root = tk.Tk()
+    root.title("Распознование символов на Python")
+    root.geometry("500x250+200+300")
+    root.show_file_dialog = show_file_dialog
+    root.choose_file = tk.Button(root,
+                                 text="Выбрать",
+                                 background="#555",
+                                 foreground="#ccc",
+                                 padx="10",
+                                 pady="4",
+                                 font="14",
+                                 command=root.show_file_dialog)
+    root.choose_file.pack()
+    filename = tk.StringVar()
+    file_entry = tk.Entry(textvariable=file_name)
+    file_entry.place()
+    root.mainloop()
 
     neuron = BIP.BmpImageFile()
 
