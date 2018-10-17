d59db78bcbb55a7ca70c10d801542972de336e1e
Evsin Roman <evsinroman@yandex.ru>
2018-10-17 21:15:23 +0300

    Подчистил файл тестов. На PyCharm модуль стал генерировать AttributeError

--- L3/L3E6.tests.py
+++ L3/L3E6.tests.py
@@ -1,17 +1,19 @@
 import unittest
-import sys
-from datetime import date as d, datetime as dt, timedelta
-import time
+from datetime import date as d, timedelta
 from L3E6 import ptrn
 
-#start_date = d(1,1,1)
-#end_date = d(9999,12,31)
-#file1 = open(r"dates1.txt", 'w')
-#file2 = open(r"dates2.txt", 'w')
-#for single_date in (start_date + timedelta(n) for n in range(int((end_date -
-#start_date).days))):
-#    file1.write(single_date.strftime("%d/%m/%Y\n"))
-#    file2.write(single_date.strftime("%d.%m.%Y\n"))
+
+def create_date_files():
+    start_date = d(1, 1, 1)
+    end_date = d(9999, 12, 31)
+    file1 = open(r"dates1.txt", 'w')
+    file2 = open(r"dates2.txt", 'w')
+    for single_date in (start_date + timedelta(n) for n in
+                        range(int((end_date -
+                                   start_date).days))):
+        file1.write(single_date.strftime("%d/%m/%Y\n"))
+        file2.write(single_date.strftime("%d.%m.%Y\n"))
+
 
 class TestRegExp(unittest.TestCase):
 
@@ -38,12 +40,12 @@ class TestRegExp(unittest.TestCase):
         self.assertFalse(ptrn.search(r"11.11.\\\\"))
 
     def test_month(self):
-        #true section
+        # true section
         self.assertTrue(ptrn.search("11.01.1111"))
         self.assertTrue(ptrn.search("11.12.1111"))
         self.assertTrue(ptrn.search("11.10.1111"))
         self.assertTrue(ptrn.search("11.09.1111"))
-        #false section
+        # false section
         self.assertFalse(ptrn.search("11.13.1111"))
         self.assertFalse(ptrn.search("11.00.1111"))
         self.assertFalse(ptrn.search("11.0.1111"))
@@ -52,14 +54,13 @@ class TestRegExp(unittest.TestCase):
         self.assertFalse(ptrn.search("11./.1111"))
         self.assertFalse(ptrn.search("11...1111"))
 
-
     def test_day(self):
-        #true section
+        # true section
         self.assertTrue(ptrn.search("01.11.1111"))
         self.assertTrue(ptrn.search("30.11.1111"))
         self.assertTrue(ptrn.search("29.02.2020"))
 
-        #fasle section
+        # false section
         self.assertFalse(ptrn.search("00.11.1111"))
         self.assertFalse(ptrn.search("0.11.1111"))
         self.assertFalse(ptrn.search("30.02.1111"))
@@ -70,15 +71,14 @@ class TestRegExp(unittest.TestCase):
         self.assertFalse(ptrn.search("ab.02.2018"))
         self.assertFalse(ptrn.search(".......02.2018"))
 
-
     def test_sep(self):
-        #true section
+        # true section
         self.assertTrue(ptrn.search("11.11.1111"))
         self.assertTrue(ptrn.search("11/11/1111"))
-        #false section
+        # false section
         self.assertFalse(ptrn.search("11.11/1111"))
         self.assertFalse(ptrn.search("11/11.1111"))
-               
+
 
 if __name__ == '__main__':
     unittest.main()
