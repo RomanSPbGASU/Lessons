027d065d3433781a5be5f4e1a3acac4646197cb9
Evsin Roman <evsinroman@yandex.ru>
2018-10-25 20:19:20 +0300

    Выполнил задание. Порядок вывода отличается от примера

--- L5/L5E5.py
+++ L5/L5E5.py
@@ -1,26 +1,10 @@
 def get_exchange(money: int):
-    if money < 1:
-        yield None
-    base_exchange = [1] * money
-    yield base_exchange
-    coins_equal = {2: [(1, 1)],
-                   5: [(2, 2, 1), (1, 1, 1, 1, 1)]}
-    coins = tuple(coins_equal)
-    for coin in coins:
-        exchange = base_exchange.copy()
-        while 1:
-            for coin_equal in coins_equal[coin]:
-                included = True
-                for c in coin_equal:
-                    included = included and c in exchange
-                if included:
-                    for c in coin_equal:
-                        exchange.remove(c)
-                    exchange.append(coin)
-                else:
-                    break
-            exchange.sort(reverse=True)
-            yield exchange
+    for i in range(0, money + 1, 1):
+        for j in range(i, money + 1, 2):
+            for k in range(j, money + 1, 5):
+                if k == money:
+                    yield [1] * i + [2] * ((j - i) // 2) + [5] * (
+                                (k - j) // 5)
 
 
 if __name__ == "__main__":
