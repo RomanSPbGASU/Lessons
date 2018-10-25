e3bb7e65472361246a4b622a8d3fd0243b3b73c5
Evsin Roman <evsinroman@yandex.ru>
2018-10-25 18:52:00 +0300

    Поменял алгоритм. Всё ещё не работает

--- L5/L5E5.py
+++ L5/L5E5.py
@@ -1,15 +1,26 @@
 def get_exchange(money: int):
     if money < 1:
-        raise ValueError
-    coins = [1, 2, 5]
-    i = 1
-    j = -1
-    while 1:
-        exchange = []
-        while sum(exchange) != money:
-            exchange.append(coins[i + j])
-        i += 1
-        yield exchange
+        yield None
+    base_exchange = [1] * money
+    yield base_exchange
+    coins_equal = {2: [(1, 1)],
+                   5: [(2, 2, 1), (1, 1, 1, 1, 1)]}
+    coins = tuple(coins_equal)
+    for coin in coins:
+        exchange = base_exchange.copy()
+        while 1:
+            for coin_equal in coins_equal[coin]:
+                included = True
+                for c in coin_equal:
+                    included = included and c in exchange
+                if included:
+                    for c in coin_equal:
+                        exchange.remove(c)
+                    exchange.append(coin)
+                else:
+                    break
+            exchange.sort(reverse=True)
+            yield exchange
 
 
 if __name__ == "__main__":
