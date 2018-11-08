9ecb9ecb7c8bb3cf92c3d46069c765a8e47485db
Evsin Roman <evsinroman@yandex.ru>
2018-11-08 14:25:55 +0300

    Выполнил 5-е задание 6-го урока

---
+++ L6/L6E5.py
@@ -0,0 +1,28 @@
+class Stack(list):
+    def __init__(self):
+        super().__init__()
+        self.pop_count = 0
+
+    def push(self, obj):
+        super().append(obj)
+        print(len(self), ": ", obj)
+
+    def pop(self, index=-1):
+        self.pop_count += 1
+        try:
+            print(self.pop_count, ": ", super().pop(0))
+        except IndexError:
+            print("Ошибка. Очередь пуста")
+
+
+if __name__ == "__main__":
+    print("Помещяем в очередь...")
+    queue = Stack()
+    queue.push(11)
+    queue.push(22)
+    queue.push(33)
+    print("Извлекаем из очереди...")
+    queue.pop()
+    queue.pop()
+    queue.pop()
+    queue.pop()
