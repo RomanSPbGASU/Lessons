b040aa78d94f8ef8b089ed72b7d2776deb48fb76
Evsin Roman <evsinroman@yandex.ru>
2018-11-11 01:08:38 +0300

    Выполнил 6-е задание 6-го урока

--- L6/L6E6.py
+++ L6/L6E6.py
@@ -1,6 +1,5 @@
 class LinkList:
-    first = None
-    last = None
+    first = last = None
     length = 0
 
     def __init__(self, *items):
@@ -8,59 +7,85 @@ class LinkList:
             self.append(item)
 
     def __str__(self):
-        res = "/"
+        res = "/ "
         for value in self:
-            res += value + "/"
-        # if len(res) == 1:
-        #     res += "/"
+            res += str(value) + " / "
         return res
-
-    # TODO: написать итератор, чтобы использовать его в методе __getitem__
-    # который использовать в методах pop и append
 
     def __next__(self):
         raise NotImplementedError("Необходимо переопределить метод")
 
     def __iter__(self):
-        raise NotImplementedError("Необходимо переопределить метод")
+        current = self.first
+        for i in range(self.length):
+            yield current.value
+            current = current.next
 
-    def __getitem__(self, index):
+    def __get_node(self, index):
         if index < 0:
             offset = self.length + index
         else:
             offset = index
+        if not 0 <= offset <= self.length:
+            raise IndexError()
         current = self.first
-        try:
-            for i in range(offset):
-                if current is None:
-                    raise IndexError()
-                current = current.next
-        except IndexError as ie:
-            print(ie)
-        current.next = current.next.next
-        return current.next
+        for i in range(offset):
+            current = current.next
+        return current
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            raise NotImplementedError()
+        else:
+            return self.__get_node(index).value
+
+    def __setitem__(self, key, value):
+        ...
+
+    def __delitem__(self):
+        ...
 
     def __len__(self):
         return self.length
 
     def append(self, data):
-        if self.first is None:
-            self.first = Node(data)
-            self.last = self.first
-            self.length += 1
-            return
-        self.last.next = Node(data)
+        if self.first:
+            self.last.next = Node(data)
+            self.last = self.last.next
+        else:
+            self.last = self.first = Node(data)
         self.length += 1
 
     def pop(self, index=-1):
-        prev = self[index - 1]
-        prev.next = self[index].next
+        if self.length == 0:
+            raise IndexError("Ошибка. Список пуст")
+        else:
+            res = self[index]
+            if self.length == 1:
+                self.first = self.last = None
+            else:
+                if index == -1:
+                    self.last = self.__get_node(index - 1)
+                    self.last.next = None
+                else:
+                    try:
+                        prev = self.__get_node(index - 1)
+                    except IndexError:
+                        self.first = self.first.next
+                    else:
+                        prev.next = self.__get_node(index).next
+
+            self.length -= 1
+            return res
 
 
 class Node:
     def __init__(self, data=None, next_node=None):
         self.value = data
         self.next = next_node
+
+    def __repr__(self):
+        return " /" + str(self.value) + "/ -->" + str(self.next)
 
 
 if __name__ == "__main__":
@@ -71,4 +96,7 @@ if __name__ == "__main__":
         print(i, ": ", link_list[-1])
     print("Извлекаем из списка...")
     for i in range(1, 5):
-        print(i + 1, ": ", link_list.pop())
+        try:
+            print(i, ": ", link_list.pop())
+        except IndexError as ie:
+            print(ie)
