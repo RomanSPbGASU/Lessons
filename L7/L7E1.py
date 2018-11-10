a3c77912e0f93674f3c25c4cffe5b99dd160a03f
Evsin Roman <evsinroman@yandex.ru>
2018-11-11 01:44:20 +0300

    Выполнил 1-е задание 7-го урока

--- L7/L7E1.py
+++ L7/L7E1.py
@@ -1,10 +1,13 @@
 def merge_files(new_file_name, *file_names):
-    res = open(new_file_name, )
+    open(new_file_name, "w").close()
+    res = open(new_file_name, "at")
     for file_name in file_names:
         with open(file_name) as file:
-
+            res.write(file.read())
+        print(file_name, "записан в файл", new_file_name)
+    res.close()
 
 
 if __name__ == "__main__":
-    file_names = ["F101.txt", "F123.txt", "F134.txt", "F135.txt"]
-    merge_files("RES.txt", file_names)
+    names = "F101.txt", "F123.txt", "F134.txt", "F135.txt"
+    merge_files("RES.txt", *names)
