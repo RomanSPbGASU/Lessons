51b80ad2ea488a3e34f452e31d198957930fc705
Evsin Roman <evsinroman@yandex.ru>
2018-11-13 09:08:42 +0300

    Добавил обработку исключений

--- L8/L8E6.py
+++ L8/L8E6.py
@@ -1,21 +1,32 @@
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter import messagebox
 
 
 def insert_text(text_data: Text):
-    file_name = fd.askopenfilename()
-    with open(file_name) as file:
-        info = file.read()
-        text_data.insert(1.0, info)
+    try:
+        file_name = fd.askopenfilename()
+    except FileNotFoundError:
+        messagebox.showerror("Ошибка", "Не выбран файл для чтения")
+    else:
+        with open(file_name) as file:
+            info = file.read()
+            text_data.insert(1.0, info)
 
 
 def extract_text(text_data: Text):
-    file_name = fd.asksaveasfilename(filetypes=(("TXT files", "*.txt"),
-                                                ("HTML files", ".html; *.htm"),
-                                                ("All files", "*.*")))
-    with open(file_name, "w") as file:
-        info = text_data.get(1.0, END)
-        file.write(info)
+    # как добавлять расширение к введённому пользователем имени файла?
+    try:
+        file_name = fd.asksaveasfilename(
+            filetypes=(("TXT files", "*.txt"),
+                       ("HTML files", ".html; *.htm"),
+                       ("All files", "*.*")))
+    except FileNotFoundError:
+        messagebox.showinfo("Информация", "Файл не сохранён")
+    else:
+        with open(file_name, "w") as file:
+            info = text_data.get(1.0, END)
+            file.write(info)
 
 
 if __name__ == "__main__":
@@ -26,3 +37,5 @@ if __name__ == "__main__":
     open_btn.grid(row=1, sticky=E)
     save_btn = Button(text="Сохранить", command=lambda t=text: extract_text(t))
     save_btn.grid(row=1, column=1, sticky=W)
+
+    window.mainloop()
