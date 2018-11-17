c3a6b352e3e09368f8866e5e068ec8ae2fee1d59
Evsin Roman <evsinroman@yandex.ru>
2018-11-18 00:20:58 +0300

    Написал регулярное выражение (с ошибками)

--- L7/L7E4.py
+++ L7/L7E4.py
@@ -1,15 +1,28 @@
 import os
+import re
 
 if __name__ == "__main__":
     os.chdir("bank_contacts\\")
     banks_info = "banks_info.txt"
     open(banks_info, "w").close()
-    file_names = os.listdir()
+    file_names = os.listdir(".")
     file_names.remove(banks_info)
     data = []
+    regexp = re.compile(
+        r"Сокращенное фирменное наименование кредитной организации\n"
+        "(?P<name>[^0-9]+)$/*^Почтовый адрес\n(?P<address>\d{6}[.,;\s][\s\t]?"
+        "[А-Я]?[а-я]+[.,;\s][\s\t]?(ул[.]?|улица)[\s\t]?[А-Яа-я0-9_\-]"
+        "[.,;\s][\s\t]?(д[.]?|дом)[\s\t]\d+[.,;\s]((кор[.]|к[.]|стр[.])"
+        "[\s\t]\d+)?).*Телефон[:\s\t](?P<phone>[0-9+][0-9\-()]*[0-9])", re.S)
     for file_name in file_names:
-        with open(file_name, encoding="utf8") as file:
-            data.append(file.readlines())
+        with open(file_name, encoding="1251") as file:
+            match = regexp.search(file.read())
+            print(match.group("name", "address", "phone"))
+
+
+
+
+
         print("Информация из файла", file_name, "считана")
 
     print("\nСправочная информация по банкам:\n")
