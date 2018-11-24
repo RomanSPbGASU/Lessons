58d355c320d1a6513fdabb0338760051e3f680c4
Evsin Roman <evsinroman@yandex.ru>
2018-11-24 23:14:05 +0300

    Реализовал основной функционал

--- L8/L8E5.py
+++ L8/L8E5.py
@@ -25,28 +25,46 @@ class ReSaverWindow(Tk):
         self.file_content_text.grid(columnspan=3)
 
         # footer (buttons)
-        self.clear_btn = Button(self, text="Очистить")
+        self.clear_btn = Button(self, text="Очистить", command=self.clear_all)
         self.clear_btn.grid()
-        self.save_btn = Button(self, text="Сохранить")
+        self.save_btn = Button(self, text="Сохранить", command=self.write_file)
         self.save_btn.grid(row=2, column=2)
+
+    def clear_hint(self, event):
+        self.file_entry.delete(0, END)
+        self.file_entry.unbind("<FocusIn>", self.__focus_id)
 
     def browse_file(self):
         self.file_path_var.set(filedialog.askopenfilename(
-            filetypes=(("TXT files", "*.txt"), ("All files", "*.*"))))
+            filetypes=(("All files", "*.*"),
+                       ("TXT files", "*.txt"),
+                       ("HTML files", "*.html|*.htm"),
+                       ("Python files", "*.py"))))
         self.read_file("<Button-1>")
 
     def read_file(self, event):
         file_path = self.file_path_var.get()
         try:
+            if file_path == "":
+                raise ValueError()
             with open(file_path, encoding="utf-8") as file:
                 self.file_content_text.insert(0.0, file.read())
         except FileNotFoundError:
             messagebox.showerror("Ошибка",
-                                 message="Файл " + file_path + "не найден")
+                                 message="Файл " + file_path + " не найден")
+        except ValueError:
+            messagebox.showerror("Ошибка", message="Файл не выбран")
 
-    def clear_hint(self, event):
+    def clear_all(self):
         self.file_entry.delete(0, END)
-        self.file_entry.unbind("<FocusIn>", self.__focus_id)
+        self.file_content_text.delete(0.0, END)
+
+    def write_file(self, event):
+        file_path = self.file_path_var.get()
+        with open(file_path, "w") as file:
+            file.write(self.file_content_text.get(0.0, END))
+        messagebox.showinfo("Сохранение файла",
+                            message="Файл " + file_path + " сохранён")
 
     def show(self):
         self.mainloop()
