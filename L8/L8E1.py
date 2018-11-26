87ab9c0d898ac753878d90f36d2777a6a0d7f2ba
Evsin Roman <evsinroman@yandex.ru>
2018-11-26 14:29:08 +0300

    Выполнен основной функционал

--- L8/L8E1.py
+++ L8/L8E1.py
@@ -6,25 +6,27 @@ class Deposit(Tk):
     def __init__(self, master=None, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.title = "Схема начисления процентов"
+        self.results = []
+        self.period_lengths = (1, 3, 6, 12)
 
         # initial
         self.initial_label = Label(self, text="Начальная сумма, ₽:")
         self.initial_label.grid()
-        self.initial_var = StringVar()
+        self.initial_var = DoubleVar()
         self.initial_entry = Entry(self, textvariable=self.initial_var)
         self.initial_entry.grid(row=0, column=1)
 
         # rate
         self.rate_label = Label(self, text="Размер ставки, %:")
         self.rate_label.grid()
-        self.rate_var = StringVar()
+        self.rate_var = DoubleVar()
         self.rate_entry = Entry(self, textvariable=self.rate_var)
         self.rate_entry.grid(row=1, column=1)
 
         # term
         self.term_label = Label(self, text="Срок инвестирования, лет:")
         self.term_label.grid()
-        self.term_var = StringVar()
+        self.term_var = DoubleVar()
         self.term_entry = Entry(self, textvariable=self.term_var)
         self.term_entry.grid(row=2, column=1)
 
@@ -37,22 +39,64 @@ class Deposit(Tk):
         self.scheme_table.heading(2, text="Ежеквартально")
         self.scheme_table.heading(3, text="Раз в полгода")
         self.scheme_table.heading(4, text="Ежегодно")
-        self.scheme_table.column(1, width=90)
-        self.scheme_table.column(2, width=90)
-        self.scheme_table.column(3, width=90)
-        self.scheme_table.column(4, width=90)
-        self.scheme_table.insert("", index=END, text="Начальная сумма, ₽")
-        self.scheme_table.insert("", index=END, text="Конечная сумма, ₽")
-        self.scheme_table.insert("", index=END, text="Доход, руб")
-        self.scheme_table.insert("", index=END, text="Доходность, % год.")
+        self.scheme_table.column(1, width=95)
+        self.scheme_table.column(2, width=95)
+        self.scheme_table.column(3, width=95)
+        self.scheme_table.column(4, width=95)
+        self.initial_item = self.scheme_table.insert("", index=END,
+                                                     text="Начальная сумма, ₽")
+        self.final_item = self.scheme_table.insert("", index=END,
+                                                   text="Конечная сумма, ₽")
+        self.income_item = self.scheme_table.insert("", index=END,
+                                                    text="Доход, руб")
+        self.yield_item = self.scheme_table.insert("", index=END,
+                                                   text="Доходность, % год.")
 
         # buttons
         self.button_frame = Frame(self)
         self.button_frame.grid(row=0, rowspan=3, column=2)
         self.clear_button = Button(self.button_frame, text="Отчистить")
         self.clear_button.grid()
-        self.calculate_button = Button(self.button_frame, text="Рассчитать")
+        self.calculate_button = Button(self.button_frame, text="Рассчитать",
+                                       command=self.calculate)
         self.calculate_button.grid()
+
+    def clear(self):
+        self.initial_var.set("")
+        self.rate_var.set("")
+        self.term_var.set("")
+        self.scheme_table.delete(self.initial_item,
+                                 self.final_item,
+                                 self.income_item,
+                                 self.yield_item)
+
+    def calc_final(self, period):
+        init = self.initial_var.get()
+        rate = self.rate_var.get()
+        term = self.term_var.get()
+        return init * (1 + rate / 100 * period / 12) ** (term * 12 / period)
+
+    def calc_income(self, final):
+        return float(final) - self.initial_var.get()
+
+    def calc_yield(self, income):
+        income = float(income)
+        if not income:
+            return 0.
+        else:
+            return income / self.initial_var.get() / self.term_var.get() * 100
+
+    def calculate(self):
+        set_item = self.scheme_table.item
+        set_item(self.initial_item, values=[self.initial_var.get()] * 4)
+        set_item(self.final_item,
+                 values=list(map(self.calc_final, self.period_lengths)))
+        final_values = self.scheme_table.item(self.final_item)["values"]
+        set_item(self.income_item,
+                 values=list(map(self.calc_income, final_values)))
+        income_values = self.scheme_table.item(self.income_item)["values"]
+        set_item(self.yield_item,
+                 values=list(map(self.calc_yield, income_values)))
 
     def show(self):
         self.mainloop()
@@ -61,36 +105,3 @@ class Deposit(Tk):
 if __name__ == "__main__":
     deposit = Deposit()
     deposit.show()
-    indict = {"initial": None, "rate": None, "term": None}
-    func_in_list = [lambda: float(input("Введите начальную сумму, ₽ = ")),
-                    lambda: float(input("Введите размер ставки, % = ")),
-                    lambda: int(input("Введите срок инвестирования, лет = "))]
-    i = 0
-    for key in indict:  # добьёмся от пользователя ввода корректных данных
-        while 1:
-            try:
-                indict[key] = func_in_list[i]()
-                i += 1
-                break
-            except ValueError:
-                print("\tОшибка. Некорректный ввод")
-    print("\t\t\t\t\tСхема начисления процентов")
-    print("\t\t\t\t\t\tЕжемесячно     Ежеквартально  Раз в полгода  Ежегодно")
-    per_len = [1, 3, 6, 12]
-    lines = [["Начальная сумма, ₽:", lambda: indict["initial"], [None] * 4],
-             ["Конечная сумма, ₽:", lambda: indict["initial"] * (
-                     1 + indict["rate"] / 100 * per_len[i] / 12) ** (
-                                                    indict["term"] * 12 /
-                                                    per_len[i]),
-              [None] * 4],
-             ["Доход, руб:", lambda: lines[1][2][i] - indict["initial"],
-              [None] * 4],
-             ["Доходность, % год.:",
-              lambda: lines[2][2][i] / indict["initial"] / indict[
-                  "term"] * 100, [None] * 4]]  # данные
-    for title, func, output in lines:  # движок
-        print(format(title, "22"), end="\t")
-        for i in range(4):
-            output[i] = func()
-            print(format(output[i], "<15.2f"), end="")
-        print()
