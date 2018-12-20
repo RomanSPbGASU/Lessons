7fe1e55e96ac8a3576b74054eddbca6c835f90d0
Роман Евсин <a1n1o1@yandex.ru>
2018-12-20 23:23:05 +0300

    Начал выполнять задание

---
+++ СV_L/CV_L4.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+
+class GraphWindow(Tk):
+    def __init__(self):
+        super().__init__()
+
+# Вариант №8
+# Эмитент	Доля, %
+# Уралкалий	25
+# Акрон	30
+# Фосагро	20
+# Дорогобуж	10
+# НКНХ	15
